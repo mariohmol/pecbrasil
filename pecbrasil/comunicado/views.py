531ebd70a3e40d2c29899d601bfdbd7ec649d543
Mario Mol <mariohmol@gmail.com>
2014-05-06 18:46:25 -0300

    comunicado sem time

--- pecbrasil/comunicado/views.py
+++ pecbrasil/comunicado/views.py
@@ -168,7 +168,7 @@ def criartime(time_id=None):
                     print "Unexpected error:"
                     print "Error EMail"
             total=total+1
-            log=log+","+[row[0]]
+            log=log+","+str([row[0]])
                 
     
     return render_template("comunicado/statuscomunicado.html",titulo=titulo,total=total,log=log)    
