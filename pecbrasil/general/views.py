0f088eb07932d40e1dca0069488f175cb157de8b
Mario Mol <mariohmol@gmail.com>
2016-11-14 01:11:51 -0200

    new server

--- pecbrasil/general/views.py
+++ pecbrasil/general/views.py
@@ -17,7 +17,7 @@ politicaServices = PoliticaServices()
 # ---------------------------
 @app.before_request
 def before_request():
-    
+
     g.timing = []
     g.timing.append(time.time())
     g.color = "#af1f24"
@@ -25,35 +25,35 @@ def before_request():
     g.latest_year = {}
     for dataset in __latest_year__:
         g.latest_year[dataset] = __latest_year__[dataset]
-    
+
     t1 = time.time()
-    
+
     # Check if the user has access (temp log in for development purposes)
     if 'has_access' not in session:
         session['has_access'] = False
-    
+
     # test if the user has access!
     if not session['has_access'] and request.endpoint and request.endpoint != "static":
         session['has_access'] = True
- 
+
     if 'rodada_atual' not in session:
         rodadaA = politicaServices.rodadaAtual()
         session['rodada_atual']  = rodadaA
 #        if request.endpoint == "general.home":
 #            form = AccessForm()
-            
+
 #            if "pw" in request.form:
-                
+
 #                if request.form["pw"] == "parabens":
 #                    session['has_access'] = True
-                    
+
 #                else:
 #                    return render_template("general/access.html", form=form)
 #            else:
 #                return render_template("general/access.html", form=form)
 #        else:
 #            return redirect(url_for("general.home"))
-            
+
     # Save variable in session so we can determine if this is the user's
     # first time on the site
     if 'first_time' in session:
@@ -61,24 +61,24 @@ def before_request():
     else:
         session['first_time'] = True
         # flash("I've noticed it's your first time on the site. Welcome!")
-    
-    
+
+
     if request.args.get('invitepromo') is not None:
         session['invitepromo']=request.args.get('invitepromo')
-        
+
     # Check if the user is logged in, if so give the global object
     # a reference to the user from DB
     g.user = current_user
-    
-    if g.user.is_authenticated() and request.endpoint != 'static' and not request.is_xhr:
+
+    if g.user abd g.user.is_authenticated() and request.endpoint != 'static' and not request.is_xhr:
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
-    
+
     # Set the locale to either 'pt' or 'en' on the global object
     if request.endpoint != 'static':
         g.locale = get_locale()
-    
+
     t2 = time.time()
     g.timing.append("Global Before Request: {0:.4f}s".format(t2-t1))
 
@@ -116,7 +116,7 @@ def get_locale(lang=None):
             new_lang = session['locale']
         else:
             session['locale'] = new_lang
-    
+
     return new_lang
 
 @babel.timezoneselector
@@ -126,7 +126,7 @@ def get_timezone():
         return user.timezone
 
 ###############################
-# General views 
+# General views
 # ---------------------------
 @app.after_request
 def after_request(response):
@@ -147,10 +147,10 @@ def home():
     rodada_atual = db.session.merge(session['rodada_atual'])
     if g.user is not None and g.user.is_authenticated() \
         and ('user_time' not in session or session['user_time']) :
-        timeRetorno = politicaServices.meuTime(g.user.id) 
+        timeRetorno = politicaServices.meuTime(g.user.id)
         if timeRetorno is None:
             return   redirect(url_for('politica.criarPartido'))
-    
+
     rodadaAtual= politicaServices.rodadaAtual()
     top3Time= politicaServices.top3Time()
     topPol= politicaServices.topPoliticosRodada(None,10)
@@ -158,7 +158,7 @@ def home():
     primeiro=None
     segundo=None
     terceiro=None
-    for line in top3Time:       
+    for line in top3Time:
         num_lines += 1
         if num_lines == 1:
             primeiro = line
@@ -179,7 +179,7 @@ def access():
     return redirect(url_for('general.home'))
 
 ###############################
-# Set language views 
+# Set language views
 # ---------------------------
 @mod.route('set_lang/<lang>/')
 def set_lang(lang):
@@ -199,6 +199,6 @@ def page_not_found(e):
     sabrina["outfit"] = "lab"
     sabrina["face"] = "scared"
     sabrina["hat"] = None
-    
-    return render_template('general/404.html', 
+
+    return render_template('general/404.html',
         error = e, sabrina = sabrina), 404
--- visual.wsgi
+++ visual.wsgi
@@ -1,5 +1,6 @@
 import sys
-sys.path.insert(0, '/home/politica/git/pecbrasil/')
+sys.path.insert(0, '/home/peclube/public_html/')
+sys.executable = '/usr/bin/python2.7'
 
 from pecbrasil import app as application
 
